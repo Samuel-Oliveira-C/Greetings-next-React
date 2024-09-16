01208a7420915b6f6196e85659b59ca074fd4a24
Samuel Oliveira <samuel.oleiv@gmail.com>
2024-09-16 19:46:09 -0300

    Criado o componente.tsx

---
+++ greeting-app/src/Components/DataGreeting.tsx
@@ -0,0 +1,50 @@
+const Horario = () =>{
+    const hour = new Date();
+    const data = hour.getFullYear();
+    const month = hour.getMonth();
+    const day = hour.getDay();
+    
+    const mes = `${day}/${month+1}/${data}`;
+
+    const hora = hour.getHours();
+    const minutes = hour.getMinutes();
+    const horario = `${hora}:${minutes}`
+
+
+    
+    if(hora > 5 && hora < 12){
+        return(
+            <div className="flex flex-col gap-5">
+                <div className="text-5xl text-pink-50">{mes}</div> 
+                <br />
+                <div className="text-3xl text-pink-50">{horario}</div>
+                <br />
+                <div className= "">{`Bom dia`}</div>
+            </div>
+        )
+    }
+    else if(hora >= 12 && hora < 18){
+        return(
+            <div className="flex flex-col gap-5">
+                <div className="text-5xl text-pink-50">{mes} </div> 
+                
+                <div className="text-3xl text-pink-50">{horario}</div>
+                
+                <div className= "font-bold text-lg">{`Boa Tarde`}</div>
+            </div>
+        )
+    }
+    else{
+        return(
+            <div className="flex flex-col gap-5">
+                <div className="text-5xl text-pink-50">{mes} </div> 
+                
+                <div className="text-3xl text-pink-50">{horario}</div>
+                
+                <div className= "">{`Boa Noite`}</div>
+            </div>
+        )
+    }
+}
+
+export {Horario}
